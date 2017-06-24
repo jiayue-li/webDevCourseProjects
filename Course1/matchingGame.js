edcfb5a1392dac765ed4b5e5b0e9d32b9270fe5d
jiayue-li <jiayue.li@berkeley.edu>
2017-06-24 14:31:14 -0700

    finished week3 git add -A

--- Course1/matchingGame.js
+++ Course1/matchingGame.js
@@ -1,9 +1,30 @@
 var numberOfFaces = 5;
-var theLeftSide = document.getElementById("leftSide")
+var theLeftSide = document.getElementById("leftSide");
+var theRightSide = document.getElementById("rightSide");
+var body = document.getElementsByTagName("body")[0];
+
+function startGame(){
+  generateFaces();
+  addHandlers();
+}
+
+function addHandlers(){
+  theLeftSide.lastChild.onclick = function continueToNextLevel(){
+    event.stopPropagation();
+    numberOfFaces += 5;
+    clearFaces();
+    generateFaces();
+    addHandlers();
+  };
+  body.onclick = function gameOver(){
+    alert("GAME OVER!!")
+    body.onclick = null;
+    theLeftSide.lastChild.onclick = null;
+  }
+}
 
 function generateFaces(){
-  theLeftSide.backgroundColor = "blue";
-  for (var count in numberOfFaces){
+  for (var i = 0; i < numberOfFaces; i++){
     var face = document.createElement("img");
     face.src = "http://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png";
     var topPx = Math.floor(Math.random()*401);
@@ -12,4 +33,15 @@ function generateFaces(){
     face.style.left = leftPx + "px";
     theLeftSide.appendChild(face);
   }
+
+  var leftSideFacesCopy = theLeftSide.cloneNode(true);
+  leftSideFacesCopy.removeChild(leftSideFacesCopy.lastChild);
+  theRightSide.appendChild(leftSideFacesCopy);
+}
+
+function clearFaces(){
+  while (theLeftSide.firstChild){
+    theLeftSide.removeChild(theLeftSide.lastChild);
+  }
+  theRightSide.removeChild(theRightSide.lastChild);
 }
