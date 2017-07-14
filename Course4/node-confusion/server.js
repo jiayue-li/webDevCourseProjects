236127dfffe33edbcf9fc39153186cd5ca5edb6a
jiayue-li <jiayue.li@berkeley.edu>
2017-07-13 22:54:53 -0700

    finished w1 and w2 c4

---
+++ Course4/node-confusion/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+var dishRouter = require('./dishRouter.js');
+var promoRouter = require('./promoRouter.js');
+var leaderRouter = require('./leaderRouter.js')
+//specifies dishRouter to only apply on /dishes
+app.use('/dishes',dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leadership', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
